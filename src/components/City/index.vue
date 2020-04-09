26cd2911acb04959d8e7b741994812e24ce2108e
张美霞 <xia15773071507@163.com>
2020-04-09 22:43:12 +0800

    add setData

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,90 +1,116 @@
-<template>
+、<template>
     <div class="city_body">
         <div class="city_list">
             <div class="city_hot">
                 <h2>热门城市</h2>
                 <ul class="clearfix">
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
+                    <li :key="item.id" v-for="item in hotList">{{item.nm}}</li>
                 </ul>
             </div>
-            <div class="city_sort">
-                <div>
-                    <h2>A</h2>
+            <div class="city_sort" ref="city_sort">
+                <div :key="item.index" v-for="item in cityList">
+                    <h2>{{item.index}}</h2>
                     <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>A</h2>
-                    <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>A</h2>
-                    <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
+                        <li :key="itemList.id" v-for="itemList in item.list">{{itemList.nm}}</li>
                     </ul>
                 </div>	
             </div>
         </div>
         <div class="city_index">
             <ul>
-                <li>A</li>
-                <li>B</li>
-                <li>C</li>
-                <li>D</li>
-                <li>E</li>
+                <li :key="item.index" v-for="(item,i) in cityList" @touchstart="handleToIndex(i)">{{item.index}}</li>
             </ul>
         </div>
     </div>
 </template>
 <script>
 export default {
-    name:'City'
+    name:'City',
+    data(){
+        return {
+            cityList:[],
+            hotList:[]
+        }
+    },
+    mounted(){
+        this.$axios.get('/api/cityList').then((res)=>{
+            var msg = res.data.msg;
+            if(msg === 'ok'){
+                var cities = res.data.data.cities;
+                var { cityList, hotList } = this.formatCityList(cities);
+                this.cityList = cityList;
+                this.hotList = hotList;
+            }
+        });
+    },
+    methods:{
+        // 获取城市数据
+        formatCityList(cities){
+            var cityList = [];
+            var hotList = [];
+
+            // 热门城市
+            for(var i=0;i<cities.length;i++){
+                if(cities[i].isHot === 1){
+                    hotList.push(cities[i]);
+                }
+            }
+
+            // 城市
+            for(var i=0;i<cities.length;i++){
+                // 城市的第一个字母
+                var firstLetter = cities[i].py.substring(0,1).toUpperCase();
+                if(toCom(firstLetter)){ // 新添加index，index为字母A-Z
+                    cityList.push({
+                        index:firstLetter,
+                        list:[{
+                            nm:cities[i].nm,
+                            id:cities[i].id
+                        }]
+                    });
+                }else{ // 累加到已有index中
+                    for(var j=0;j<cityList.length;j++){
+                        if(cityList[j].index === firstLetter){
+                            cityList[j].list.push({
+                                nm:cities[i].nm,
+                                id:cities[i].id
+                            });
+                        }
+                    }
+                }
+            }
+
+            // 排序
+            cityList.sort((n1,n2)=>{
+                if(n1.index > n2.index){
+                    return 1;
+                }else if(n1.index < n2.index){
+                    return -1;
+                }else{
+                    return 0;
+                }
+            })
+
+            function toCom(firstLetter){
+                for(var i=0;i<cityList.length;i++){
+                    if(cityList[i].index === firstLetter){
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            return {
+                cityList,
+                hotList
+            };
+        },
+        // 点击字母事件
+        handleToIndex(index){
+            var h2 = this.$refs.city_sort.getElementsByTagName('h2');
+            this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop;
+        }
+    }
 }
 </script>
 <style scoped>
